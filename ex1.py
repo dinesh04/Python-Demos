a6f1649c0ddfb6790a06f75189bdc9017ac36dd1
dinesh04 <dineshbhosale04@gmail.com>
2016-12-16 19:52:05 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,11 @@
+print "Hello world "
+
+# print "Hello again";
+
+print "I like typing this"
+
+print 'yes still printing'
+
+print 'I siad "Do" nt touch this'
+
+print " I'd much better you ' not"
